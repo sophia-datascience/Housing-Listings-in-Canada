0c16d759ed584c94f52ec30ff4e68f69f2cab480
sophia-datascience <sophia.datascience@gmail.com>
2024-10-30 22:03:37 -0400

    Add files via upload

---
+++ Real Estate Listings in Canada.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM [Real Estate in Canada]
+
+SELECT City, Province, Bedrooms, Bathrooms, ROUND(AVG(Price), 2) AS Average_Price, COUNT(*) AS Total_listings
+FROM [Real Estate in Canada]
+GROUP BY City, Province, Bedrooms, Bathrooms
+ORDER BY Average_Price, Total_listings  DESC;
+
+SELECT *
+FROM [Real Estate in Canada]
+WHERE Province = 'ON' AND Bedrooms > 2;
+
+SELECT *
+FROM [Real Estate in Canada]
+WHERE price < 300000
+ORDER BY price ASC;
+
+
+SELECT TOP 10 *
+FROM [Real Estate in Canada]
+ORDER BY price DESC;
